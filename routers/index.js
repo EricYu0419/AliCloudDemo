14f1c17beb9135e5ff5b6204105df47988e3602e
Eric Yu <ericy0419@gmail.com>
2018-05-08 10:44:52 +0800

    fix code for public work

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,6 @@
 const Router = require("koa-router");
 const v1 = require("./v1");
 const router = new Router();
-router.get("/", async (ctx, next) => {
-  await next();
-  ctx.body = "hello world";
-});
 
 router.use("/v1", v1.routes(), v1.allowedMethods());
 
