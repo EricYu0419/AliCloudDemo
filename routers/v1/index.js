e8bd84480e40ee7dc1c5368b73618dd14941d41b
Eric Yu <ericy0419@gmail.com>
2018-05-08 15:19:22 +0800

    v1 api update commit

--- routers/v1/index.js
+++ routers/v1/index.js
@@ -2,8 +2,9 @@ const Router = require("koa-router");
 const cfg = require("../../config").alicloud;
 const ALY = require("aliyun-sdk");
 const router = new Router();
-const ecs = require("./ecs");
-const ess = require("./ess");
+const apis = require("fs").readdirSync(
+  require("path").join(__dirname + "../../../node_modules/aliyun-sdk/apis")
+);
 // const AcsClient = new AcsROAClient(cfg);
 
 router.get("/apiList", async (ctx, next) => {
@@ -12,7 +13,63 @@ router.get("/apiList", async (ctx, next) => {
   await next();
 });
 
-router.use("/ecs", ecs.routes(), ecs.allowedMethods());
-router.use("/ess", ess.routes(), ess.allowedMethods());
+// console.info(cfg, apis);
+cfg.apiList.forEach(element => {
+  if (element) {
+    let apiFileName = "";
+    apis.forEach(fileName => {
+      if (fileName.indexOf(element) === 0) apiFileName = fileName;
+    });
+    const apiJson = require(`aliyun-sdk/apis/${apiFileName}`);
+    let apiVersion = apiFileName
+      .replace(element + "-", "")
+      .replace(".json", "");
+    // console.info(apiFileName,apiVersion);
+    const alyApi = new ALY[(element.toLocaleUpperCase())]({
+      accessKeyId: cfg.accessKeyId,
+      secretAccessKey: cfg.accessKeySecret,
+      endpoint: `https://${element}.aliyuncs.com`,
+      apiVersion: apiVersion
+    });
+    const apiRouter = new Router();
+
+    function ToPromise(ctxParams, ctxQuery) {
+      return new Promise(function(resolve, reject) {
+        let action = ctxParams.Action;
+        let params = {};
+        let tempP = ctxQuery || ctxParams;
+        Object.keys(tempP).forEach(element => {
+          if (element !== "Action") params[element] = tempP[element];
+        });
+        alyApi[action](params, function(err, res) {
+          // console.info(err,res);
+          if (err) reject(err);
+          resolve(res);
+        });
+      });
+    }
+
+    apiRouter
+      .get("/", async (ctx, next) => {
+        await next();
+        ctx.body = apiJson;
+      })
+      .get("/:Action", async (ctx, next) => {
+        await ToPromise(ctx.params, ctx.query).then(res => {
+          // console.info(res);
+          ctx.body = res;
+          next();
+        });
+      })
+      .get("/:Action/:RegionId", async (ctx, next) => {
+        await ToPromise(ctx.params).then(res => {
+          ctx.body = res;
+          next();
+        });
+      });
+
+    router.use(`/${element}`, apiRouter.routes(), apiRouter.allowedMethods());
+  }
+});
 
 module.exports = router;
