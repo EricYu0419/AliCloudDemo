802ead0f571f2c38bfb3984b3f426d4ca90f0c75
Eric Yu <ericy0419@gmail.com>
2018-05-10 18:03:56 +0800

    update app.js commit

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const serve = require("koa-static");
 const proxy = require("koa-proxy");
 const logger = require("koa-logger");
 const cors = require("koa-cors");
-const compose = require("koa-compose");
 const cfg = require("./config");
 const router = require("./routers");
-const bodyParser = require('koa-bodyparser');
+const bodyParser = require("koa-bodyparser");
 const app = new Koa();
 
+// 允许跨域
 app.use(cors());
-app.use(bodyParser());
 
+// 日志过滤资源文件
 function ignoreAssets(mw) {
   return async function(ctx, next) {
-    if (/(\.js|\.css|\.ico)$/.test(ctx.path)) {
+    if (/(\.js|\.css|\.ico|\.jpg|\.png|\.gif|\.bmp)$/.test(ctx.path)) {
       await next();
     } else {
       // must .call() to explicitly set the receiver
@@ -23,29 +23,28 @@ function ignoreAssets(mw) {
   };
 }
 
-// x-response-time
-
-async function responseTime(ctx, next) {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  ctx.set("X-Response-Time", ms + "ms");
-}
-
-// composed middleware
-
-const middlewares = compose([responseTime, ignoreAssets(logger())]);
+// 将public目录是为静态请求地址
 app.use(serve("./public"));
-// console.info(router);
-app.use(middlewares);
+app.use(ignoreAssets(logger()));
+
+// 根据配置代理信息选择是处理还是转发。
 if (!cfg.proxy) {
-  console.info('proxy mode close')
+  console.info("非代理模式（全栈一体）");
+  app.use(bodyParser());
   app.use(router.routes()).use(router.allowedMethods());
 } else {
-  console.info('proxy mode start')
-  app.use(proxy({
-    host:cfg.proxy.host
-  }));
+  console.info(
+    `代理模式：Proxy = > ${
+      cfg.proxy.host
+    } (静态站点调用本地，API调用代理服务器)`
+  );
+  app.use(
+    proxy({
+      host: cfg.proxy.host
+    })
+  );
 }
 
-app.listen(8080);
+// 根据配置监听端口
+app.listen(cfg.port);
+console.info(`服务启动于 127.0.0.1:${cfg.port}`);
